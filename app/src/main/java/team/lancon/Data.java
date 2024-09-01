00724675f8c895e37aa1f5c6a2c2d0651c4082f3
IsrakAhmed <israkahmed7@gmail.com>
2024-09-01 18:07:13 +0600

    Data class updated to support file and userList transfer

--- app/src/main/java/team/lancon/Data.java
+++ app/src/main/java/team/lancon/Data.java
@@ -1,12 +1,54 @@
 package team.lancon;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.HashMap;
 
-public class Data implements Serializable,Cloneable{
+public class Data implements Serializable, Cloneable {
 
     public String message;
+    private List<HashMap<String, Object>> userList;
 
-    public Object clone()throws CloneNotSupportedException{
+    // Fields for file transfer
+    private String fileName;
+    private long fileSize;
+    private String fileType;
+    private byte[] fileData;
+
+    public void setUserList(List<HashMap<String, Object>> userList) {
+        this.userList = userList;
+    }
+
+    public List<HashMap<String, Object>> getUserList() {
+        return userList;
+    }
+
+    // File transfer methods
+    public void setFileData(String fileName, long fileSize, String fileType, byte[] fileData) {
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.fileType = fileType;
+        this.fileData = fileData;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public byte[] getFileData() {
+        return fileData;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
 }
